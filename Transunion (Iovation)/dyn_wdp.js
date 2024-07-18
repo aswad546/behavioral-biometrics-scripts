75eea7e927dc349b5afe5d4bc7a544f3e73da621
Aswad Tariq <86660510+aswad546@users.noreply.github.com>
2024-07-18 12:00:32 -0400

    Added missing Iovation script

---
+++ Transunion (Iovation)/dyn_wdp.js
@@ -0,0 +1,70 @@
+//Found on https://www.expedia.ca/login?ckoflag=0&uurl=e3id%3Dredr%26rurl%3D%2F July 18th 2024
+/*
+ Copyright(c) 2020 TransUnion LLC. All Rights Reserved. 80808bae-d4c3-47ac-9929-5d77264d823c
+*/
+(function g() {
+    function a(a, b) {
+        b && (f.nuidIndex.push(a),
+        f[a] = b)
+    }
+    var d = window || this
+      , b = d.io_global_object_name || "IGLOO"
+      , c = d[b] = d[b] || {};
+    b = c.fp = c.fp || {};
+    var e = b.api || {}
+      , f = {
+        nuidIndex: []
+    };
+    d = d.decodeURIComponent;
+    if (c.loader && !1 === c.loader.fp_dyn || b.dynMain)
+        return !1;
+    b.dynMain = g;
+    e && (a("jstoken", "zDSC7x3mdvkbLYZDpwFZXkBUI3Q1X/QoYUOh7Gz/5cs="),
+    e.decode && (c = e.decode,
+    b.ripServerUrl = c("bXBzbmFyZS5pZXNuYXJlLmNvbQ=="),
+    b.contentServerHost = c(""),
+    b.contentServerPath = c("d2RwLWZpcnN0LXNlcnZpY2UvcmVzb3VyY2VzL3N0YXRpYw=="),
+    b.ctokenScriptPath = c("aW9qcy9sYXRlc3QvbG9nby5qcw=="),
+    b.rtcServerList = c("c3R1bjpzdHVuLmwuZ29vZ2xlLmNvbToxOTMwMixzdHVuOnN0dW4zLmwuZ29vZ2xlLmNvbToxOTMwMixzdHVuOnN0dW4yLmwuZ29vZ2xlLmNvbToxOTMwMixzdHVuOnN0dW4uc3R1bnByb3RvY29sLm9yZzozNDc4LHN0dW46bnVtYi52aWFnZW5pZS5jYTozNDc4LHN0dW46c3R1bi52aXZveC5jb206MzQ3OCxzdHVuOnN0dW4uc2lwLnVzOjM0Nzgsc3R1bjpzdHVuLmNvbW1wZWFrLmNvbTozNDc4LHN0dW46c3R1bi5iYXJyYWN1ZGEuY29tOjM0Nzgsc3R1bjpzdHVuLmVweWdpLmNvbTozNDc4"),
+    a("jssrc", c("bG9jYWxob3N0OjgwODI="))),
+    a("jsmbr", ""),
+    a("haccchr", d("")),
+    a("hacclng", d("")),
+    a("suagt", d("Apache-HttpClient%2F4.5.13%20(Java%2F11.0.19)").slice(0, 400)),
+    a("svrtime", "2024/07/18 15:48:17"),
+    a("iggy", ""),
+    a("did", "pghgHjMbEvWMhaPmUGvSS8a7eCpYEqvJbNGQaaR71AQ4fKLin6VE+mgqJKfjlcqcIONGZz45n0gR25GpeOiXpA=="),
+    a("xreqw", d("")),
+    a("alias", ""),
+    a("fpremad", ""),
+    a("fphcctrl", ""),
+    a("fphclip", ""),
+    a("fphxcclip", ""),
+    a("fphcmfrm", ""),
+    a("fphxcmfrm", ""),
+    a("fphconxn", "vD8YQHnJeW8Cn2A/PM9v45W1wu+pGkv7+Xl7iFN8G68="),
+    a("fphcshnfo", ""),
+    a("fphfwded", ""),
+    a("fphxfwded", ""),
+    a("fphfwdfr", ""),
+    a("fphxfwdfr", ""),
+    a("fphprgma", ""),
+    a("fphprxcon", ""),
+    a("fphprxy", ""),
+    a("fphvia", ""),
+    a("fphxrip", ""),
+    a("tlsc", ""),
+    a("tlsv", ""),
+    a("jsver", "5.4.0"),
+    a("svrvr", "1.22.5.2"),
+    a("skey", ""),
+    a("chbrvr", ""),
+    a("charch", ""),
+    a("chplat", ""),
+    a("chplatv", ""),
+    a("chmob", ""),
+    a("chua", ""),
+    a("chmodel", ""),
+    e.ds_cb && e.ds_cb(f))
+}
+)();
